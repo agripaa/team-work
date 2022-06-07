68592ee693bc6de8b8a8c1663dc61444e1a344f9
Jounnyy <agrieva.xananda08@gmail.com>
2022-06-07 14:26:55 +0700

    add about page

--- app.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('#mobile-menu');
-const listMenu = document.querySelector('.nav-menu');
-
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    listMenu.classList.toggle('active');
-})
